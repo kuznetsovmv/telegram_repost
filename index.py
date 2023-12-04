f34b8975f6fdc48ce95a13c7fb7c877a9ef44b0e
kuznetsovmv <mihasiksli82@gmail.com.com>
2023-12-04 16:04:03 +0300

    init

---
+++ index.py
@@ -0,0 +1,41 @@
+from telethon import TelegramClient, events
+import json
+
+SESSION_NAME = 'telegram_re_poster'
+
+
+def client_start():
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+    with TelegramClient(session=SESSION_NAME, api_hash=config['api_hash'], api_id=config['api_id']).start() as client:
+        print('telegram connected')
+
+        def filter_handle(event):
+            try:
+                return event.to_id.channel_id not in config['recipient_channel_ids']
+            except:
+                return False
+
+        async def forward(event):
+            for channel_id in config['recipient_channel_ids']:
+                await client.forward_messages(channel_id, event.message)
+
+        @client.on(events.NewMessage(func=filter_handle))
+        async def handler(event):
+            try:
+                channel_id = event.to_id.channel_id
+                try:
+                    channel = await event.client.get_entity(channel_id)
+                except ValueError:
+                    return
+                if channel is None:
+                    print('no_channel')
+                    return
+                await forward(event)
+            except Exception as e:
+                print('ERROR: ', e, event)
+
+        client.run_until_disconnected()
+
+
+client_start()
