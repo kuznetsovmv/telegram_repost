43cf11c1981ba6cdf5109ebb985152fa5aadff46
kuznetsovmv <mihasiksli82@gmail.com.com>
2023-12-21 11:39:40 +0300

    added log_to_file option

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import sys
 from telethon import TelegramClient
 from telethon.events import NewMessage
 from telethon.tl.types import PeerChannel
@@ -9,12 +10,21 @@ DEFAULT_SESSION_NAME = 'telegram_forward'
 DEFAULT_CONFIG_FILE_NAME = 'config.json'
 
 
-def client_start(session_name=DEFAULT_SESSION_NAME, config_file_name=DEFAULT_CONFIG_FILE_NAME):
-    logging.basicConfig(
-        level=logging.INFO,
-        filename=f'{session_name}.log',
-        format='%(asctime)s %(levelname)s %(message)s'
-    )
+def client_start(session_name=DEFAULT_SESSION_NAME, config_file_name=DEFAULT_CONFIG_FILE_NAME, log_to_file=False):
+    log_format = '%(asctime)s %(levelname)s %(message)s'
+    log_level = logging.INFO
+    if log_to_file:
+        logging.basicConfig(
+            level=log_level,
+            filename=f'{session_name}.log',
+            format=log_format
+        )
+    else:
+        logging.basicConfig(
+            stream=sys.stdout,
+            level=log_level,
+            format=log_format
+        )
     logger = logging.getLogger()
 
     with open(config_file_name) as config_file:
@@ -56,7 +66,8 @@ def client_start(session_name=DEFAULT_SESSION_NAME, config_file_name=DEFAULT_CON
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Settings for')
-    parser.add_argument("--session", type=str, default=DEFAULT_SESSION_NAME)
-    parser.add_argument("--config", type=str, default=DEFAULT_CONFIG_FILE_NAME)
+    parser.add_argument("--session", type=str, default=DEFAULT_SESSION_NAME, required=False)
+    parser.add_argument("--config", type=str, default=DEFAULT_CONFIG_FILE_NAME, required=False)
+    parser.add_argument("--log_to_file", choices=[0, 1], type=int, default=0, required=False)
     args = parser.parse_args()
-    client_start(session_name=args.session, config_file_name=args.config)
+    client_start(session_name=args.session, config_file_name=args.config, log_to_file=args.log_to_file)
